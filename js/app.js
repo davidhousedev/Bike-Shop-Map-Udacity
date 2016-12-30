8e80097e7b71a9c433564f0ba77e8a0a46faf261
David House <davidhousedev@gmail.com>
2016-12-30 10:25:04 -0800

    feat: Display hard-coded bike shop data on an html page from a knockoutjs observable array

---
+++ js/app.js
@@ -0,0 +1,45 @@
+
+$(function(){
+    'use strict';
+
+    var ListMarker = function(data) {
+        this.lat = ko.observable(data.lat || null);
+        this.lon = ko.observable(data.lon || null);
+        this.name = ko.observable(data.name || null);
+        this.elevation = ko.observable(data.elevation || null);
+        this.placeID = ko.observable(data.placeID || null);
+        this.address = ko.observable(data.address || null);
+    };
+
+    var ViewModel = function(markers) {
+        var self = this;
+        self.markerData = ko.observableArray(markers.map(function (marker) {
+            return new ListMarker(marker);
+        }));
+
+        console.log(self.markerData())
+        /*// Creates a marker in the list and the map
+        self.makeMarker = function(){
+
+        };*/
+    };
+
+    var markers = [
+        {
+            name: 'Wheelie wheels',
+            placeID: 'abc123'
+        },
+        {
+            name: 'Bikie bike',
+            placeID: 'abc456'
+        },
+        {
+            name: 'Wompy womp',
+            placeID: 'abc987'
+        }
+    ];
+
+    var viewModel = new ViewModel(markers || []);
+    ko.applyBindings(viewModel);
+
+});
